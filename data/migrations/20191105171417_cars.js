77cc3ad557961a72040eac02ff3aa4320cb17458
Dale Pancho <dalepancho7@gmail.com>
2019-11-05 20:57:01 -0800

    MVP.

--- data/migrations/20191105171417_cars.js
+++ data/migrations/20191105171417_cars.js
@@ -3,12 +3,12 @@ exports.up = function(knex) {
     return knex.schema.createTable('cars', function(table) {
         table.increments();
 
-        table.string('VIN', 64).notNullable();
-        table.string('Make', 64).notNullable();
-        table.string('Model', 64).notNullable();
-        table.integer('Milage', 64).notNullable();
-        table.string('Transmission', 64);
-        table.string('TitleStatus', 64);
+        table.string('VIN').notNullable();
+        table.string('Make').notNullable();
+        table.string('Model').notNullable();
+        table.integer('Milage').notNullable();
+        table.string('Transmission');
+        table.string('TitleStatus');
 
         table.timestamps(true, true);
     })
